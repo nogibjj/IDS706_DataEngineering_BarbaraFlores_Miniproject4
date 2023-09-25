68119141f20ad7b0666f42ce291fad9a33be6da7
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-25 03:11:50 +0000

    changes

--- src/main.py
+++ src/main.py
@@ -18,13 +18,13 @@ def count_variable(path, variable):
 
 if __name__ == "__main__":
 
-    path = 'data/LinkedInTechJobsDataset.csv'
+    ruta = 'data/LinkedInTechJobsDataset.csv'
     variable_list = ["Total_applicants", "Employee_count", "LinkedIn_Followers"]
 
     results = dict()
     for i in variable_list:
         results[i] = { 
-            "mean" : mean_variable(path, i) ,
-            "median" : median_variable(path, i) , 
-            "count" : count_variable(path, i) }
+            "mean" : mean_variable(ruta, i) ,
+            "median" : median_variable(ruta, i) , 
+            "count" : count_variable(ruta, i) }
     print(results)
